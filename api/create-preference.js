1fa0fab8eb5c4703659e5ac684ec06fa9b0f7eb8
Diegoung <Diegomfragnito@gmail.com>
2026-07-27 19:15:55 -0300

    actualizar6

--- api/create-preference.js
+++ api/create-preference.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({
       message: "Method not allowed"
@@ -57,7 +66,8 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       id: data.id,
-      init_point: data.init_point
+      init_point: data.init_point,
+      sandbox_init_point: data.sandbox_init_point
     });
 
   } catch (error) {
