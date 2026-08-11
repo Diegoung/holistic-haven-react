5273b7460f02620026adf3df8b17300c61f6883e
Diegoung <Diegomfragnito@gmail.com>
2026-08-11 17:44:15 -0300

    actualizar6

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
 import Services from '@/components/Services';
-import Gallery from '@/components/Gallery';
+import Gallery from '@/components/EspacioHolistico';
 import Testimonials from '@/components/Testimonials';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
