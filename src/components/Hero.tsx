6b7f677caf815a715a2e98e5f1460c1f75c524a1
Diegoung <Diegomfragnito@gmail.com>
2026-09-22 18:03:47 -0300

    actualizar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ const Hero: React.FC = () => {
     <section 
       className="relative w-full min-h-[50vh] sm:min-h-[70vh] md:min-h-screen bg-[#e8eada] bg-center bg-no-repeat bg-contain md:bg-cover transition-all duration-300"
       style={{
-        backgroundImage: 'url(/terapiasholisticas1.jpg)',
+        backgroundImage: 'url(/terapiasholisticas11.jpg)',
       }}
     >
       {/* Muestra la imagen completa sin recortar nada en el celular */}
